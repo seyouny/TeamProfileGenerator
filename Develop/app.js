f9f57c962b06e432e4b6deec025e0c5a0ab6dbd6
Seyoung Yoon <stacey.exp@gmail.com>
2020-07-15 23:47:47 -0700

    edited

--- Develop/app.js
+++ Develop/app.js
@@ -54,7 +54,7 @@ function runPrompt(){
                 ]).then((re)=>{
                     phone = re.phone
                 })
-                const manager = new Manager (response.name, response.id, response.email, phone)
+                const manager = new Employee (response.name, response.id, response.email, phone)
                 employeeList.push(manager)
     
         }
@@ -66,7 +66,7 @@ function runPrompt(){
                     name: "school"
                 }
                 ])
-                const intern = new Intern (response.name, response.id, response.email, response.phone)
+                const intern = new Employee (response.name, response.id, response.email, response.phone)
                 employeeList.push(intern)
     
         }
@@ -78,13 +78,12 @@ function runPrompt(){
                     name: "github"
                 }
                 ])
-                const engineer = new Engineer (response.name, response.id, response.email, response.phone)
+                const engineer = new Employee (response.name, response.id, response.email, response.phone)
                 employeeList.push(engineer)
         }
     })
-
 }
-// *** PSUEDO CODE BECAUSE I DON'T UNDERSTAND WHERE MY MISTAKES ARE
+runPrompt().then(()=>{console.log(employeeList)})
 // *** I CREATED AN EMPLOYEELIST AND DEPENDING ON EACH CHOICE, IT SHOULD REDIRECT TO CORRECT ROLE.
 // *** THE LAST STEP I NEED IS RENDERING...
 // *** I LOOKED AT THE RENDERING HTML JS FILE FOR CLUES.
@@ -95,4 +94,39 @@ function runPrompt(){
 //     }
 // })
 
+// fs.writeFile(outputPath,render(employeeList),(err)=>{
+//     if (err){
+//         console.log("There is an error.")
+//     }
+// })
+
 runPrompt();
+// const run = async () => {
+//     const answers = await runPrompt();
+
+//     return answers
+//     console.log(employeeList)
+// };
+// run()
+
+// then push to array containing all employees
+// then render([arr])
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
